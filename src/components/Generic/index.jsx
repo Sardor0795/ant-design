03d31f01b7250e68b2e841a01043c0a79566c65a
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-18 14:04:17 +0500

    Prepare folders

---
+++ src/components/Generic/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+const Generic = () => {
+  return (
+    <Container>
+      <h1>Generic</h1>
+    </Container>
+  );
+};
+
+export default Generic;
---
+++ src/utils/navbar.jsx
@@ -0,0 +1,5 @@
+export const navbar = [
+  { id: 1, title: "Docs", path: "/docs" },
+  { id: 2, title: "Components", path: "/components" },
+  { id: 3, title: "Resources", path: "/resources" },
+];
