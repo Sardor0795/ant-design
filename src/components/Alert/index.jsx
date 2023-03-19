2be40a3ecfc616cd150f34a71d3065a973bd876a
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-19 19:00:53 +0500

    Begin alert

--- src/components/Alert/index.jsx
+++ src/components/Alert/index.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import Card from "../Card";
 import { LayoutContainer, LayoutWrapper } from "../Layout/style";
+import { AlertList, AlertText, AlertTitle } from "./style";
 
 export const Alert = () => {
   return (
-    <LayoutWrapper>
-      <LayoutContainer>
-        <Card></Card>
-      </LayoutContainer>
-      <LayoutContainer>
-        <Card></Card>
-      </LayoutContainer>
-    </LayoutWrapper>
+    <>
+      <AlertTitle>Alert</AlertTitle>
+      <AlertText>Alert component for feedback.</AlertText>
+      <AlertTitle>When To Use</AlertTitle>
+      <AlertList>
+        <li>When you need to show alert messages to users.</li>
+        <li>
+          When you need a persistent static container which is closable by user
+          actions.
+        </li>
+      </AlertList>
+
+      <LayoutWrapper>
+        <LayoutContainer>
+          <Card></Card>
+        </LayoutContainer>
+        <LayoutContainer>
+          <Card></Card>
+        </LayoutContainer>
+      </LayoutWrapper>
+    </>
   );
 };
