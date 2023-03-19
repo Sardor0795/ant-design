958eba1d53b21a929f00122fe4bf020669f0f317
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-19 17:12:47 +0500

    Add search btn

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 import LoadImg from "../../assets/icons/loader.png";
 import { LoadImage } from "./Button/style";
 import DownloadImg from "../../assets/icons/btn_download.svg";
+import SearchImg from "../../assets/icons/btn_search.svg";
 
 const Buttons = () => {
   return (
@@ -162,8 +163,112 @@ const Buttons = () => {
             Download
           </Button>
         </Card>
-        <Card type="Danger" desc={"Danger Button"}>
-          <Button type="danger">Danger</Button>
+        <Card
+          type="Loading"
+          desc={
+            "Ant Design supports a default button size as well as a large and small size. If a large or small button is desired, set the size property to either large or small respectively. Omit the size property for a button with the default size."
+          }
+        >
+          <Button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            type="warning"
+          >
+            <LoadImage style={{ display: "block" }} src={LoadImg} alt="img" />{" "}
+            Loading
+          </Button>
+          <Button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            width="110px"
+            type="warning"
+          >
+            <LoadImage style={{ display: "block" }} src={LoadImg} alt="img" />{" "}
+            Loading
+          </Button>
+          <Button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            width="small"
+            type="warning"
+          >
+            <LoadImage style={{ display: "block" }} src={LoadImg} alt="img" />
+            Loading
+          </Button>
+        </Card>
+        <Card
+          type="Search Button"
+          desc={
+            "Ant Design supports a default button size as well as a large and small size. If a large or small button is desired, set the size property to either large or small respectively. Omit the size property for a button with the default size."
+          }
+        >
+          <Button type="info">
+            <img
+              style={{
+                width: "20px",
+                filter: `grayscale(0) invert(1)`,
+                marginRight: "10px",
+              }}
+              src={SearchImg}
+              alt="img"
+            />
+            Search
+          </Button>
+          <Button
+            style={{ fontSize: "0", borderRadius: "50%" }}
+            width="40px"
+            height="40px"
+            type="info"
+          >
+            <img
+              style={{
+                width: "15px",
+                filter: `grayscale(0) invert(1)`,
+                marginRight: "0",
+              }}
+              src={SearchImg}
+              alt="img"
+            />
+            Download
+          </Button>
+          <Button
+            style={{ fontSize: "0", borderRadius: "15px" }}
+            width="40px"
+            height="40px"
+            type="info"
+          >
+            <img
+              style={{
+                width: "15px",
+                filter: `grayscale(0) invert(1)`,
+                marginRight: "0",
+              }}
+              src={SearchImg}
+              alt="img"
+            />
+            Download
+          </Button>
+          <Button style={{ fontSize: "0" }} width="50px" type="info">
+            <img
+              style={{
+                width: "15px",
+                filter: `grayscale(0) invert(1)`,
+                marginRight: "0",
+              }}
+              src={SearchImg}
+              alt="img"
+            />
+            Download
+          </Button>
         </Card>
       </Container>
     </Wrapper>
