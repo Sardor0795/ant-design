e67ff37a59f7bccc332414da78f0ad27b5a48edb
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-20 07:11:25 +0500

    Add normal success input

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Card from "../Card";
 import { LayoutContainer, LayoutWrapper } from "../Layout/style";
+import { NormalDanger } from "./normal_danger";
 import { NormalDefault } from "./normal_default";
+import { NormalSuccess } from "./normal_success";
 import { InputsList, InputsText, InputsTitle } from "./style";
 
 export const Inputs = () => {
@@ -25,6 +27,16 @@ export const Inputs = () => {
             <NormalDefault placeholder="Normal Default Small" type="default" size="small"/>
             <NormalDefault placeholder="Normal Default Medium" type="default" size="medium"/>
             <NormalDefault placeholder="Normal Default Large" type="default" size="large"/>
+          </Card>
+          <Card type="Normal Danger" desc={"type='normal' button size='large, medium, small' placeholder='normal medium'"}>
+            <NormalDanger placeholder="Normal Danger Small" type="danger" size="small"/>
+            <NormalDanger placeholder="Normal Danger Medium" type="danger" size="medium"/>
+            <NormalDanger placeholder="Normal Danger Large" type="danger" size="large"/>
+          </Card>
+          <Card type="Normal Success" desc={"type='normal' button size='large, medium, small' placeholder='normal medium'"}>
+            <NormalSuccess placeholder="Normal Success Small" type="success" size="small"/>
+            <NormalSuccess placeholder="Normal Success Medium" type="success" size="medium"/>
+            <NormalSuccess placeholder="Normal Success Large" type="success" size="large"/>
           </Card>
         </LayoutContainer>
         <LayoutContainer>
