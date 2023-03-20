af24854d1eea41fef8b23c3622abb8bbc8bc012a
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-20 06:49:40 +0500

    Add normal input

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import Card from "../Card";
 import { LayoutContainer, LayoutWrapper } from "../Layout/style";
+import { NormalDefault } from "./normal_default";
+import { InputsList, InputsText, InputsTitle } from "./style";
 
 export const Inputs = () => {
   return (
-    <LayoutWrapper>
-      <LayoutContainer>
-        <Card></Card>
-      </LayoutContainer>
-      <LayoutContainer>
-        <Card></Card>
-      </LayoutContainer>
-    </LayoutWrapper>
+    <>
+      <InputsTitle>Input</InputsTitle>
+      <InputsText>To get user input</InputsText>
+      <InputsTitle>When To Use</InputsTitle>
+      <InputsText>
+        A basic widget for getting the user input is a text field. Keyboard and
+        mouse can be used for providing or changing data.
+      </InputsText>
+
+      <InputsList>
+        <li>A user input in a form field is needed.</li>
+      </InputsList>
+
+      <LayoutWrapper>
+        <LayoutContainer>
+          <Card type="Normal Default" desc={"type='normal' button size='large, medium, small' placeholder='normal medium'"}>
+            <NormalDefault placeholder="Normal Default Small" type="default" size="small"/>
+            <NormalDefault placeholder="Normal Default Medium" type="default" size="medium"/>
+            <NormalDefault placeholder="Normal Default Large" type="default" size="large"/>
+          </Card>
+        </LayoutContainer>
+        <LayoutContainer>
+          <Card></Card>
+        </LayoutContainer>
+      </LayoutWrapper>
+    </>
   );
 };
