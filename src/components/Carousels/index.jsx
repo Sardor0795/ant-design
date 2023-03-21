547b962a2fd098c8f1eef20a4750adc09a01f4f8
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-21 06:56:16 +0500

    Prepare slider intro img

--- src/components/Carousels/index.jsx
+++ src/components/Carousels/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { CarouselImg, CarouselImgsWrapper, CarouselPageWrapper, CarouselTitle } from "./style";
+
+import CImg1 from "../../assets/img/c_1.jpg";
+import CImg2 from "../../assets/img/c_2.jpg";
+import CImg3 from "../../assets/img/c_3.jpg";
 
 export const Carousel = () => {
   return (
-    <div>
-
-            
-
-    </div>
-  )
-}
+    <CarouselPageWrapper>
+      <CarouselImgsWrapper>
+        <CarouselImg src={CImg1} alt="img" />
+        <CarouselImg src={CImg2} alt="img" />
+        <CarouselImg style={{ marginTop: "25px" }} src={CImg3} alt="img" />
+      </CarouselImgsWrapper>
+      <CarouselTitle>All my sliders made in pure HTML&CSS and JS</CarouselTitle>
+      
+    </CarouselPageWrapper>
+  );
+};
--- src/components/Carousels/style.jsx
+++ src/components/Carousels/style.jsx
@@ -1,2 +1,21 @@
 const { default: styled } = require("styled-components");
 
+export const CarouselPageWrapper = styled.div``;
+
+export const CarouselImgsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-bottom: 35px;
+`;
+
+export const CarouselImg = styled.img`
+  max-width: 400px;
+  border-radius: 4px;
+`;
+
+export const CarouselTitle = styled.div`
+  font-weight: bold;
+  font-size: 25px;
+  text-align: center;
+`;
