cfd4c270020f1999d72f271ef7e307dfe4743545
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-03-22 14:11:59 +0500

    Fix swich colors

--- src/components/Switchs/index.jsx
+++ src/components/Switchs/index.jsx
@@ -96,45 +96,97 @@ export const Switch = () => {
           </Card>
           <Card
             type="on and off"
-            desc={"on and off switch accepts textOn, textOff, size, bgChecked, bgNotChecked props"}
+            desc={
+              "on and off switch accepts textOn, textOff, size, bgChecked, bgNotChecked props"
+            }
           >
             <div>
-              <SwitchComp leftInfo="on" rightInfo="off" onClick={toggleSwitchLarge} size="large" />
-              <SwitchComp leftInfo="on" rightInfo="off" onClick={toggleSwitchMedium} size="medium" />
-              <SwitchComp leftInfo="on" rightInfo="off" onClick={toggleSwitchSmall} size="small" />
+              <SwitchComp
+                leftInfo="on"
+                rightInfo="off"
+                onClick={toggleSwitchLarge}
+                size="large"
+              />
+              <SwitchComp
+                leftInfo="on"
+                rightInfo="off"
+                onClick={toggleSwitchMedium}
+                size="medium"
+              />
+              <SwitchComp
+                leftInfo="on"
+                rightInfo="off"
+                onClick={toggleSwitchSmall}
+                size="small"
+              />
             </div>
           </Card>
+          <Card
+            type="with icon"
+            desc={
+              "tick and cross switch accepts size, bgChecked, bgNotChecked props"
+            }
+          >
+            <div>
+              <SwitchComp
+                leftInfo="✔"
+                rightInfo="✖"
+                onClick={toggleSwitchLarge}
+                size="large"
+              />
+              <SwitchComp
+                leftInfo="✔"
+                rightInfo="✖"
+                onClick={toggleSwitchMedium}
+                size="medium"
+              />
+              <SwitchComp
+                leftInfo="✔"
+                rightInfo="✖"
+                onClick={toggleSwitchSmall}
+                size="small"
+              />
+            </div>
+          </Card>
+        </LayoutContainer>
+        <LayoutContainer>
           <Card
             type="Mini Project"
             desc={"Light mode / Dark mode project using swich"}
           >
-            <SwichMiniProject/>
+            <SwichMiniProject />
           </Card>
-        </LayoutContainer>
-        <LayoutContainer>
-        <Card
-            type="with icon"
-            desc={"tick and cross switch accepts size, bgChecked, bgNotChecked props"}
-          >
-            <div>
-              <SwitchComp leftInfo="✔" rightInfo="✖" onClick={toggleSwitchLarge} size="large" />
-              <SwitchComp leftInfo="✔" rightInfo="✖" onClick={toggleSwitchMedium} size="medium" />
-              <SwitchComp leftInfo="✔" rightInfo="✖" onClick={toggleSwitchSmall} size="small" />
-            </div>
-          </Card>
-        <Card
+
+          <Card
             type="1 and 0"
             desc={"1 and 0 switch accepts size, bgChecked, bgNotChecked props"}
           >
             <div>
-              <SwitchComp leftInfo="1" rightInfo="0" onClick={toggleSwitchLarge} size="large" />
-              <SwitchComp leftInfo="1" rightInfo="0" onClick={toggleSwitchMedium} size="medium" />
-              <SwitchComp leftInfo="1" rightInfo="0" onClick={toggleSwitchSmall} size="small" />
+              <SwitchComp
+                leftInfo="1"
+                rightInfo="0"
+                onClick={toggleSwitchLarge}
+                size="large"
+              />
+              <SwitchComp
+                leftInfo="1"
+                rightInfo="0"
+                onClick={toggleSwitchMedium}
+                size="medium"
+              />
+              <SwitchComp
+                leftInfo="1"
+                rightInfo="0"
+                onClick={toggleSwitchSmall}
+                size="small"
+              />
             </div>
           </Card>
-        <Card
+          <Card
             type="Size"
-            desc={"basic three sizes switch accepts size, bgChecked, bgNotChecked props"}
+            desc={
+              "basic three sizes switch accepts size, bgChecked, bgNotChecked props"
+            }
           >
             <div>
               <SwitchComp onClick={toggleSwitchSmall} size="small" />
@@ -142,13 +194,23 @@ export const Switch = () => {
               <SwitchComp onClick={toggleSwitchLarge} size="large" />
             </div>
           </Card>
-        <Card
+          <Card
             type="Loading"
-            desc={"loading switch accepts loading, size, bgChecked, bgNotChecked props"}
+            desc={
+              "loading switch accepts loading, size, bgChecked, bgNotChecked props"
+            }
           >
             <div>
-              <SwitchComp load="load" onClick={toggleSwitchLarge} size="large" />
-              <SwitchComp load="load" onClick={toggleSwitchMedium} size="medium" />
+              <SwitchComp
+                load="load"
+                onClick={toggleSwitchLarge}
+                size="large"
+              />
+              <SwitchComp
+                load="load"
+                onClick={toggleSwitchMedium}
+                size="medium"
+              />
               <SwitchComp onClick={toggleSwitchSmall} size="small" />
             </div>
           </Card>
